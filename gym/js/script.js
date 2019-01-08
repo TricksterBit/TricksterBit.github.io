c88a6608f51ff528c1469e2ab28ffe12bf5c8db3
TricksterBit <denchikm95@gmail.com>
2019-01-08 16:11:52 +1000

    Add files via upload

---
+++ gym/js/script.js
@@ -0,0 +1,39 @@
+$(function() {
+
+	$(document).ready(function() {
+  
+		var width = 300;
+		
+		//dimensions
+		$('.slides>li').width(width);
+		// $('.slides').width(width*$('.slides>li').length);
+		
+		//positioning
+		 $('.slides').css('left',-width);
+		 $('.slides>li:last-child').prependTo('.slides');
+		
+		//move slides forward
+		function nextSlide() {
+		  $('.slides').animate({
+			'margin-left':-width
+		  },500, function() {
+			$('.slides>li:first-child').appendTo('.slides');
+			$('.slides').css('margin-left', 0);
+		  });
+		}
+		//move slides backwards
+		function prevSlide() {
+		  $('.slides').animate({
+			'margin-left':width
+		  },500, function() {
+			$('.slides>li:last-child').prependTo('.slides');
+			$('.slides').css('margin-left', 0);
+		  });
+		}
+		  
+		//controls
+		$('.next').click(nextSlide);
+		$('.prev').click(prevSlide)
+	  });
+
+});
